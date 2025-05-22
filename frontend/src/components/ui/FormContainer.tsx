46ba9d7d1d209625722dedb8a832ebaad8421d92
marcopaxman <marcopaxman@gmail.com>
2025-05-22 17:09:07 +0200

    Altered all previous components
    Changed all the previous components to include the their dark theme colours.

--- frontend/src/components/ui/FormContainer.tsx
+++ frontend/src/components/ui/FormContainer.tsx
@@ -7,7 +7,7 @@ interface FormContainerProps {
 
 const FormContainer: React.FC<FormContainerProps> = ({ onSubmit, children }) => {
     return (
-        <form onSubmit={onSubmit} className="space-y-6 p-8 bg-white shadow-xl rounded-lg w-full max-w-md">
+        <form onSubmit={onSubmit} className="space-y-6 p-8 bg-white dark:bg-dark-div shadow-xl rounded-lg w-full max-w-md">
             {children}
         </form>
     );
