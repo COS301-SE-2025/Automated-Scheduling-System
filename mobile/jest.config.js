36fd9e8cb0c4e471737f67b3970d9c2279a02236
ethanwilke23 <ethanwilke69@gmail.com>
2025-09-27 17:05:27 +0200

    unit testing for Button.tsx

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   preset: 'jest-expo',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupTests.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-native-big-calendar|@react-native-async-storage/async-storage|expo(nent)?|@expo|expo-router)/)'
-  ],
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
